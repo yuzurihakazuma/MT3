fa5f04e871d3dd85f2329596fcec9af583fbf707
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-04-21 10:55:48 +0900

    a

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -6,3 +6,11 @@ struct Matrix4x4 {
 	float m;
 };
 
+namespace MatrixMath {
+
+
+
+	// 3次元アフィン変換行列
+	Matrix4x4 MakeAffine(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+}
