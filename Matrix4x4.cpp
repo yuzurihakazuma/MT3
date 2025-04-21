7065ce0e86daf596722f5aea2b7813dfadb55179
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-04-21 12:34:02 +0900

    a

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -18,12 +18,33 @@ Matrix4x4 MatrixMath::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
     return result;
 }
 
+// 平行移動行列
 Matrix4x4 MatrixMath::MakeTranslate(const Vector3& translate) {
-	return Matrix4x4();
-}
 
+	Matrix4x4 result = {};
+	// 単位行列の作成
+	result.m[0][0] = 1.0f;
+	result.m[1][1] = 1.0f;
+	result.m[2][2] = 1.0f;
+	result.m[3][3] = 1.0f;
+	// 平行移動の成分
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+
+	return result;// 完成した平行移動を返す
+}
+// 拡大縮小行列
 Matrix4x4 MatrixMath::MakeScale(const Vector3& scale) {
-	return Matrix4x4();
+
+	Matrix4x4 result = {};
+	// 拡大率の設定
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+
+	return result;// 拡大(スケ―リンク)行列を返す
 }
 
 Matrix4x4 MatrixMath::MakeRotateX(float radian) {
