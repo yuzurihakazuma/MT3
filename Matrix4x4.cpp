9ae86f0d85f41ee4f19ab447cc5b87848a6e6d1b
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-04-21 12:29:42 +0900

    a

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -1,2 +1,29 @@
 #include "Matrix4x4.h"
 
+Matrix4x4 MatrixMath::Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeTranslate(const Vector3& translate) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeScale(const Vector3& scale) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeRotateX(float radian) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeRotateY(float radian) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeRotateZ(float radian) {
+	return Matrix4x4();
+}
+
+Matrix4x4 MatrixMath::MakeAffine(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	return Matrix4x4();
+}
