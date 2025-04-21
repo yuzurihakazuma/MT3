85888dba101d759116565f7d71859c395c21f21e
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-04-21 11:11:58 +0900

    a

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -8,7 +8,10 @@ struct Matrix4x4 {
 
 namespace MatrixMath {
 
-
+	// 平行移動行列
+	Matrix4x4 MakeTranslate(const Vector3& translate);
+	// 拡大縮小行列
+	Matrix4x4 MakeScale(const Vector3& scale);
 
 	// 3次元アフィン変換行列
 	Matrix4x4 MakeAffine(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
