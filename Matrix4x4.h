e73161ae9268d85eabbc9718e138fb458b596ad8
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-07 16:51:09 +0900

    a

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -25,6 +25,12 @@ namespace MatrixMath {
 	// 3次元アフィン変換行列
 	Matrix4x4 MakeAffine(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
 
+	// 正射影行列
+	Matrix4x4 Orthographic(float left, float top, float right, float bottom, float nearClip, float farClip);
+	// 透視投影行列
+	Matrix4x4 PerspectiveFov(float fovY, float aspectRatio, float nearClip, float farClip);
+	// ビューポート変換行列
+	Matrix4x4 Viewport(float left, float top, float width, float height, float minDepth, float maxDepth);
 
 
 }
