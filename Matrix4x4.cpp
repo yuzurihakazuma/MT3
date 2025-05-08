eebe73267d4fd3a51512b615d6e9831ed161b9e0
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-09 08:53:47 +0900

    a

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -182,3 +182,53 @@ Matrix4x4 MatrixMath::Inverse(const Matrix4x4& m) {
 
     return result;
 }
+
+// 正射影行列
+Matrix4x4 MatrixMath::Orthographic(float left, float top, float right, float bottom, float nearClip, float farClip) {
+
+    Matrix4x4 result = {};
+
+    result.m[0][0] = 2.0f / (right - left);
+    result.m[1][1] = 2.0f / (top - bottom);
+    result.m[2][2] = 1.0f / (farClip - nearClip);
+    result.m[3][0] = (left + right) / (left - right);
+    result.m[3][1] = (top + bottom) / (bottom - top);
+    result.m[3][2] = nearClip / (nearClip - farClip);
+    result.m[3][3] = 1.0f;
+
+
+    return result;
+}
+// 透視投影行列
+Matrix4x4 MatrixMath::PerspectiveFov(float fovY, float aspectRatio, float nearClip, float farClip) {
+
+    Matrix4x4 result = {};
+
+    // コタンジェント(fovY/2)
+    float f = 1.0f / std::tan(fovY / 2.0f);
+
+    // 透視投影行列の各要素設定
+    result.m[0][0] = f / aspectRatio;
+    result.m[1][1] = f;
+    result.m[2][2] = farClip / (farClip - nearClip);
+    result.m[2][3] = 1.0f;
+    result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+    return result;
+}
+// ビューポート変換行列
+Matrix4x4 MatrixMath::Viewport(float left, float top, float width, float height, float minDepth, float maxDepth) {
+
+    Matrix4x4 result = {};
+
+    result.m[0][0] = width / 2.0f;
+    result.m[1][1] = -height / 2.0f;
+    result.m[2][2] = maxDepth - minDepth;
+    result.m[3][0] = left + (width / 2.0f);
+    result.m[3][1] = top + (height / 2.0f);
+    result.m[3][2] = minDepth;
+    result.m[3][3] = 1.0f;
+
+    return result;
+}
+
