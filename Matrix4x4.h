e8885de3cec95d79afaca587328affaed4998839
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-09 16:09:48 +0900

    a

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -9,6 +9,13 @@ struct Matrix4x4 {
 namespace MatrixMath {
 
 
+	// 正射影行列
+	Matrix4x4 Orthographic(float left, float top, float right, float bottom, float nearClip, float farClip);
+	// 透視投影行列
+	Matrix4x4 PerspectiveFov(float fovY, float aspectRatio, float nearClip, float farClip);
+	// ビューポート変換行列
+	Matrix4x4 Viewport(float left, float top, float width, float height, float minDepth, float maxDepth);
+
 
 
 }
