565dcd5ddf6651e4d2bfe1bbd2659ebc9d5b94bf
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-09 16:10:44 +0900

    a

--- Matrix4x4.cpp
+++ Matrix4x4.cpp
@@ -1,4 +1,69 @@
 #include "Matrix4x4.h"
 
+// 座標変換
+Vector3 MatrixMath::Transform(const Vector3& vector, const Matrix4x4& matrix) {
 
+	Vector3 result = {};
+	// 座標の変換(行ベクトルx行列の列)
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f); //wが0にならないようにする
+
+	// wで割って通常の3次元空間に戻す
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;// 変換後のベクトルを返す
+}
+
+// 正射影行列
+Matrix4x4 MatrixMath::Orthographic(float left, float top, float right, float bottom, float nearClip, float farClip) {
+
+	Matrix4x4 result = {};
+
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 1.0f / (farClip - nearClip);
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	result.m[3][3] = 1.0f;
+
+
+	return result;
+}
+// 透視投影行列
+Matrix4x4 MatrixMath::PerspectiveFov(float fovY, float aspectRatio, float nearClip, float farClip) {
+
+	Matrix4x4 result = {};
+
+	// コタンジェント(fovY/2)
+	float f = 1.0f / std::tan(fovY / 2.0f);
+
+	// 透視投影行列の各要素設定
+	result.m[0][0] = f / aspectRatio;
+	result.m[1][1] = f;
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+	return result;
+}
+// ビューポート変換行列
+Matrix4x4 MatrixMath::Viewport(float left, float top, float width, float height, float minDepth, float maxDepth) {
+
+	Matrix4x4 result = {};
+
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + (width / 2.0f);
+	result.m[3][1] = top + (height / 2.0f);
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
 
