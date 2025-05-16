c328866f0317d653d7d7b2f99e40d8a0dd274ad0
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-16 16:50:58 +0900

    a

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -8,6 +8,10 @@ struct Matrix4x4 {
 
 namespace MatrixMath {
 
+
+	// 行列の積
+	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
 	// 平行移動行列
 	Matrix4x4 MakeTranslate(const Vector3& translate);
 	// 拡大縮小行列
