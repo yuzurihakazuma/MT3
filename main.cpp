de58da32b421870c8aa74e778de3cbe0170f94ff
yuzurihakazuma <k024g0063@g.neec.ac.jp>
2025-05-30 15:59:51 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
+#include "Grid.h"
+
+using namespace MatrixMath;
+
 const char kWindowTitle[] = "LE2C_28_ユズリハ_カズマ";
 
 
@@ -28,6 +33,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		
+			// ワールド行列
+		Matrix4x4 worldMatrix = MakeAffine({ 1.0f,1.0f,1.0f }, camaraRotate, cameraTranslate);
+		// カメラのワールド行列
+		Matrix4x4 cameraMatrix = MakeAffine({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPos);
+		// ビュー行列はカメラ行列の逆行列
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		// 透視投影行列
+		Matrix4x4 projectionMatrix = PerspectiveFov(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		// W→V→Pの順番でかけ合わせる
+		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		// ビューポート行列
+		Matrix4x4 viewportMatrix = Viewport(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+
+
+
+
+
 		///
 		/// ↑更新処理ここまで
 		///
